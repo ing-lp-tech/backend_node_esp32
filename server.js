159d0d2dd9dfd7f58b8bc9730a8d15a3bb2a0820
ing-lp-tech <ing.lp.tech@gmail.com>
2024-07-22 23:19:52 -0300

    feat:first commit

---
+++ server.js
@@ -0,0 +1,76 @@
+import express from "express";
+import fetch from "node-fetch";
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Reemplaza con la IP local de tu ESP32
+const ESP32_IP = "http://192.168.1.91";
+
+app.get("/api/led/:state", async (req, res) => {
+  const state = req.params.state;
+  console.log(`Received request to turn ${state} the LED`);
+
+  try {
+    const response = await fetch(`${ESP32_IP}/?led=${state}`);
+    const text = await response.text();
+    console.log(`Response from ESP32: ${text}`);
+    res.send(text);
+  } catch (error) {
+    console.error("Error communicating with ESP32:", error.message);
+    res.status(500).send("Error communicating with ESP32: " + error.message);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+/* import express from "express";
+import fetch from "node-fetch";
+
+const app = express();
+//const ESP32_IP = "http://192.168.1.45";  // Reemplaza con la IP de tu ESP32
+const ESP32_IP = "http://192.168.0.91";
+
+app.use(express.json());
+
+app.get("/api/led/:state", async (req, res) => {
+  const state = req.params.state;
+  try {
+    const response = await fetch(`${ESP32_IP}/?led=${state}`);
+    const data = await response.text();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send("Error controlling LED");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+}); */
+
+/* const express = require("express");
+const app = express();
+const fetch = require("node-fetch"); */
+
+/* const ESP32_IP = "http://192.168.1.45"; */ // Reemplaza con la IP de tu ESP32
+
+/* const ESP32_IP = "http://192.168.0.89";
+
+app.use(express.json());
+
+app.get("/api/led/:state", async (req, res) => {
+  const state = req.params.state;
+  try {
+    const response = await fetch(`${ESP32_IP}/?led=${state}`);
+    const data = await response.text();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send("Error controlling LED");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+ */
